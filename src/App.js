97b1a416c2985b219e8116977d451c18c3d2c6ac
zarzani <zahraarzani@gmail.com>
2024-05-27 21:50:51 +0330

    adding h1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
       
         <footer> this is my git hub link <a href="https://github.com/zarzani/arzani52"  target="_blank"
           rel="noopener noreferrer"> my git hub</a> </footer>
---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+    return (
+        <div className="weather"> 
+        <h1> Tehran </h1>
+        
+        
+        </div> )
+        }
+    
