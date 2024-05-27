b74b16b716c060a8e98b391231582961bf949b62
zarzani <zahraarzani@gmail.com>
2024-05-27 11:23:05 +0330

    adding boostrap

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div>
+      <h1> Zahra Arzani</h1>
+
+      
+    </div>
   </React.StrictMode>
 );
 
