84a96f41e3589e89444083c0f96c44fceeceefc7
zarzani <zahraarzani@gmail.com>
2024-05-28 18:56:19 +0330

    adding weather

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,18 @@ export default function Weather() {
     return (
         <div className="weather"> 
         <h1> Tehran </h1>
-        
+        <ul>
+            <li>
+                wednesday 07.00
+            </li>
+            <li> mostly cloudy</li>
+        </ul>
+        <div className="row">
+            <div className="col=6">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" Mostly sunny />
+                83  °F °C
+            </div>
+        </div>
         
         </div> )
         }
