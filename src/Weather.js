a75881460529a60e43bff8248f90351443c1dad6
zarzani <zahraarzani@gmail.com>
2024-06-05 17:03:14 +0330

    how to call icon?

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./Weather.css";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherForcast from "./WeatherForcast";
 
 
 export default function Weather(props) {
@@ -83,6 +84,7 @@ export default function Weather(props) {
           </div>
           
         </div>
+        <WeatherForcast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
