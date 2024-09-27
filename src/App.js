7d1a31150fa895ee26f733be2a569ccb37b04b57
zarzani <zahraarzani@gmail.com>
2024-09-27 17:52:17 +0330

    add component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Weather from "./Weather";
 import './App.css';
+import Hello from "./Hello";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <Weather city="New York" />
+        <Hello />
       
         <footer> this is my git hub link <a href="https://github.com/zarzani/arzani52"  target="_blank"
           rel="noopener noreferrer"> my git hub</a> </footer>
